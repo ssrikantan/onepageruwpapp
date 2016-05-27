829bcdffa9201c55838c0a2976e9a67374eaa0c4
ssrikantan <s_srikantan@hotmail.com>
2016-05-27 17:10:04 +0530

    removed certain config parameters

--- ProductPortfolioUWPApp/ProductPortfolioUWPApp/Util/Constants.cs
+++ ProductPortfolioUWPApp/ProductPortfolioUWPApp/Util/Constants.cs
@@ -8,18 +8,18 @@ namespace ProductPortfolioUWPApp.Util
 {
     public class Constants
     {
-        //public readonly static string IMAGE_URL_PREFIX = "http://productportfolio.azurewebsites.net/wp-content/uploads/";
-        public readonly static string IMAGE_FEATURES_URL_PREFIX = "https://productportfolioimages.blob.core.windows.net/featureimages/";
-        public readonly static string IMAGE_PRODUCTS_URL_PREFIX = "https://productportfolioimages.blob.core.windows.net/productimages/";
+        //public readonly static string IMAGE_URL_PREFIX = "empty";
+        public readonly static string IMAGE_FEATURES_URL_PREFIX = "empty";
+        public readonly static string IMAGE_PRODUCTS_URL_PREFIX = "empty";
 
-        public readonly static string _caseStudyUrl = "https://customers.microsoft.com/Pages/advancedsearch.aspx?mrmcproducts=";
-        public readonly static string supportLifeCycleUrl = "http://support.microsoft.com/lifecycle/?LN=en-us&alpha=";
+        public readonly static string _caseStudyUrl = "empty";
+        public readonly static string supportLifeCycleUrl = "empty";
 
 
-        public readonly static string onpagerAllProductsPosterUrl = "https://productportfolioimages.blob.core.windows.net/onepagerdocs/Microsoft Products Portfolio 3.9.pdf";
-        public readonly static string onpagerAllAzurePosterUrl = "https://productportfolioimages.blob.core.windows.net/onepagerdocs/Microsoft Azure Services Overview v1.0.pdf";
-        public readonly static string onpagerPrivacyPolicyUrl = "http://productportfolioapp.azurewebsites.net/Home/PrivacyPolicy";
-        public readonly static string onpagerDisclaimerUrl = "http://productportfolioapp.azurewebsites.net/Home/Disclaimer";
+        public readonly static string onpagerAllProductsPosterUrl = "empty";
+        public readonly static string onpagerAllAzurePosterUrl = "empty";
+        public readonly static string onpagerPrivacyPolicyUrl = "empty";
+        public readonly static string onpagerDisclaimerUrl = "empty";
 
 
     }
